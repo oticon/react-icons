8f9b60bae28dee98f0d1c3cce45fac810d52b795
Sadi Kaya <skay@oticon.com>
2017-11-10 14:10:08 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Icons from './components/Icons';
+import PropTypes from 'prop-types';
+import styles from './fonts/Icons.css';
 
-render(
-  <Icons />,
-  document.getElementById('root'),
-);
+/**
+ * Icons
+ * @param color
+ * @param icon
+ * @param fontSize
+ * @returns {XML}
+ * @constructor
+ */
+export const Icons = ({ color, icon, fontSize }) => {
+  const inlineStyles = { color, fontSize };
+  return <span className={cn(styles.icomoon, styles[icon])} style={inlineStyles} />;
+};
+
+/**
+ * defaultProps
+ * @type {{color: string, icon: string, fontSize: string}}
+ */
+Icons.defaultProps = {
+  color: "#000",
+  icon: "icon-budicon-info",
+  fontSize: "24px",
+};
+
+/**
+ * propTypes
+ * @type {{color: shim, icon: *, fontSize: shim}}
+ */
+Icons.propTypes = {
+  color: PropTypes.string,
+  icon: PropTypes.string.isRequired,
+  fontSize: PropTypes.string,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,68 @@
-switch (process.env.NODE_ENV) {
-  case 'production':
-    module.exports = require('./webpack/webpack.prod');
-    break;
-  case 'development':
-  default:
-    module.exports = require('./webpack/webpack.dev');
-}
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = () => {
+  const removeEmpty = array => array.filter(p => !!p);
+
+  return {
+    entry: {
+      app: ['./src/index'],
+    },
+    devtool: 'cheap-source-map',
+    output: {
+      path: path.join(__dirname, './lib/'),
+      filename: '[name].js',
+      sourceMapFilename: '[name].map',
+    },
+    resolve: {
+      extensions: ['.js'],
+      modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'babel-loader', query: { cacheDirectory: true } }],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            { loader: 'css-loader', options: { modules: true, importLoaders: 1 }},
+            'postcss-loader'
+          ],
+        },
+        {
+          test: /\.(ico|eot|svg|ttf|woff|woff2)$/,
+          use: [{ loader: 'url-loader?name=[name].[ext]' }],
+        }
+      ],
+    },
+    plugins: removeEmpty([
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false,
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true,
+          warnings: false,
+          unused: true,
+          dead_code: true,
+          drop_console: true,
+        },
+        output: {
+          comments: false,
+        },
+        sourceMap: true,
+      }),
+    ]),
+  };
+};
