707bdde0506ac95c3b71271fe63c15a7e177e84f
Sadi Kaya <skay@oticon.com>
2017-11-13 08:20:04 +0100

    latest.

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = () => {
-  const removeEmpty = array => array.filter(p => !!p);
-
   return {
     entry: {
       app: ['./src/index'],
     },
-    devtool: 'cheap-source-map',
     output: {
       path: path.join(__dirname, './lib/'),
       filename: 'index.js',
-      sourceMapFilename: 'index.map',
     },
     resolve: {
       extensions: ['.js'],
@@ -39,30 +35,5 @@ module.exports = () => {
         }
       ],
     },
-    plugins: removeEmpty([
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false,
-      }),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production'),
-        },
-      }),
-      new webpack.optimize.ModuleConcatenationPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          screw_ie8: true,
-          warnings: false,
-          unused: true,
-          dead_code: true,
-          drop_console: true,
-        },
-        output: {
-          comments: false,
-        },
-        sourceMap: true,
-      }),
-    ]),
   };
 };
